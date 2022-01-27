91d8b7c70c4f939f4aa194453eb9a440df81f072
Taylor Galloway <galloway.taylor@gmail.com>
2022-01-27 12:13:47 -0600

    Create user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,21 @@ class ApplicationController < ActionController::API
             message: "Hello World"
         }, status: 200
     end
+
+    def not_found
+        render json: { error: 'not_found' }
+      end
+    
+    def authorize_request
+    header = request.headers['Authorization']
+    header = header.split(' ').last if header
+    begin
+        @decoded = JsonWebToken.decode(header)
+        @current_user = User.find(@decoded[:user_id])
+    rescue ActiveRecord::RecordNotFound => e
+        render json: { errors: e.message }, status: :unauthorized
+    rescue JWT::DecodeError => e
+        render json: { errors: e.message }, status: :unauthorized
+    end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
   root "application#hello"
+  resources :users, param: :_username
+  post '/auth/login', to: 'authentication#login'
+  get '/*a', to: 'application#not_found'
 end
