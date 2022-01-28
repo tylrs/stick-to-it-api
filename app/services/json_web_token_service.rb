20debfa133b69186219874f36bf8090baed6d8f6
Taylor Galloway <galloway.taylor@gmail.com>
2022-01-28 13:01:32 -0600

    Change constant name

--- app/services/json_web_token_service.rb
+++ app/services/json_web_token_service.rb
@@ -1,14 +1,14 @@
 class JsonWebTokenService
-  SECRET_KEY = Rails.application.secrets.secret_key_base.freeze
+  SECRET_KEY_BASE = Rails.application.secrets.secret_key_base.freeze
 
   def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
-    JWT.encode(payload, SECRET_KEY)
+    JWT.encode(payload, SECRET_KEY_BASE)
   end
 
   def self.decode(token)
     Rails.logger.info "Token before being decoded>>>#{token}"
-    decoded = JWT.decode(token, SECRET_KEY)[0]
+    decoded = JWT.decode(token, SECRET_KEY_BASE)[0]
     Rails.logger.info "Decoded>>>>#{decoded}"
     HashWithIndifferentAccess.new decoded
   end
