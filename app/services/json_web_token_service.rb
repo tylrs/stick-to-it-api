a9d349527d870ced830e851d1f948d19fcb26c4d
Taylor Galloway <galloway.taylor@gmail.com>
2022-01-28 13:49:43 -0600

    Change secret key assignment

--- app/services/json_web_token_service.rb
+++ app/services/json_web_token_service.rb
@@ -1,5 +1,5 @@
 class JsonWebTokenService
-  SECRET_KEY = Rails.env == "production" ? ENV["SECRET_KEY_BASE"].freeze : Rails.application.secrets.secret_key_base.freeze
+  SECRET_KEY = Rails.application.secret_key_base
 
   def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
