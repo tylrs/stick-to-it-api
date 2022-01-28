2fcb9a9374838c9f08986994fe8680590674872e
Taylor Galloway <galloway.taylor@gmail.com>
2022-01-28 16:46:26 -0600

    Return whole user from login action

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -8,7 +8,7 @@ class AuthenticationController < ApplicationController
       token = JsonWebTokenService.encode(user_id: @user.id)
       time = Time.now + 24.hours.to_i
       render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
-                     username: @user.username }, status: :ok
+                     user: @user}, status: :ok
     else
       render json: { error: 'unauthorized' }, status: :unauthorized
     end
