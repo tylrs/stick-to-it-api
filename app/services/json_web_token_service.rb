d353ca6e7dcd30f75744adeb4378dbbb9ea742c6
Taylor Galloway <galloway.taylor@gmail.com>
2022-01-28 13:10:50 -0600

    Test secret key existence

--- app/services/json_web_token_service.rb
+++ app/services/json_web_token_service.rb
@@ -1,15 +1,15 @@
 class JsonWebTokenService
-  SECRET_KEY_BASE = Rails.application.secrets.secret_key_base.freeze
+  SECRET_KEY = Rails.application.secrets.secret_key_base.freeze
 
   def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
-    JWT.encode(payload, SECRET_KEY_BASE)
+    JWT.encode(payload, SECRET_KEY)
   end
 
   def self.decode(token)
     Rails.logger.info "Token before being decoded>>>#{token}"
-    decoded = JWT.decode(token, SECRET_KEY_BASE)[0]
-    Rails.logger.info "Decoded>>>>#{decoded}"
+    Rails.logger.info "Key>>>>#{SECRET_KEY}"
+    decoded = JWT.decode(token, SECRET_KEY)[0]
     HashWithIndifferentAccess.new decoded
   end
 end
