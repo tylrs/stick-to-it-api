241030e764113afd2d675ee5dbc9174403077d2d
Taylor Galloway <galloway.taylor@gmail.com>
2022-01-31 16:21:28 -0600

    Connect user and habit model to join table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :user_habits
+  has_many :habits, through: user_habits
   # mount_uploader :avatar, AvatarUploader
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
