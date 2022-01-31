beca3287a3fe49320be5175cf0d43045c1a8de6e
Taylor Galloway <galloway.taylor@gmail.com>
2022-01-31 16:23:03 -0600

    Run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_27_181331) do
+ActiveRecord::Schema.define(version: 2022_01_31_212745) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "habits", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "start_datetime", precision: 6
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "user_habits", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "habit_id", null: false
+    t.boolean "completed"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["habit_id"], name: "index_user_habits_on_habit_id"
+    t.index ["user_id"], name: "index_user_habits_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -22,4 +41,7 @@ ActiveRecord::Schema.define(version: 2022_01_27_181331) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
+
+  add_foreign_key "user_habits", "habits"
+  add_foreign_key "user_habits", "users"
 end
