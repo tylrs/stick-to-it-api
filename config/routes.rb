a279e252e9d5007e5e4ea56f9192d98265b7f3b4
Taylor Galloway <galloway.taylor@gmail.com>
2022-01-31 16:09:12 -0600

    Add nested route for habits

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "application#hello"
-  resources :users
+  resources :users do 
+    resources :habits
+  end
   post '/auth/login', to: 'authentication#login'
   get '/*a', to: 'application#not_found'
 end
