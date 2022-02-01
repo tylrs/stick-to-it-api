e4e09fc9dc1718e2d0106bdea4be21bca0be6057
Taylor Galloway <galloway.taylor@gmail.com>
2022-02-01 09:32:37 -0600

    Add validations on habit model

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,4 +1,8 @@
 class Habit < ApplicationRecord
     has_many :user_habits
     has_many :users, through: :user_habits
+
+    validates :name, presence: true, uniqueness: true
+    validates :description, presence: true
+    validates :datetime, presence: true
 end
