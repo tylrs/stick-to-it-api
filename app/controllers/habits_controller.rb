06509f223f923c051e003c31cf1e02e4fbcf40f8
Taylor Galloway <galloway.taylor@gmail.com>
2022-02-01 11:30:35 -0600

    Remove .create! and switch instance variables

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -3,32 +3,32 @@ class HabitsController < ApplicationController
   before_action :find_user
 
   def index
-    @habits = @user.habits.all
-    render json: @habits, status: :ok  
+    habits = @user.habits.all
+    render json: habits, status: :ok  
   end
 
   def create
-    @habit = @user.habits.create!(habit_params)
-    if @habit.save
-      render json: @habit, status: :created
+    habit = @user.habits.new(habit_params)
+    if habit.save
+      render json: habit, status: :created
     else
-      render json: { errors: @habit.errors.full_messages },
+      render json: { errors: habit.errors.full_messages },
              status: :unprocessable_entity
     end
   end
 
   def show
-    @habit = @user.habits.find_by id: params[:id]
-    render json: @habit, status: :ok
+    habit = @user.habits.find_by id: params[:id]
+    render json: habit, status: :ok
   end
 
   def update
-    @habit = @user.habits.find_by id: params[:id]
-    @habit.update(habit_params)
-    if @habit.valid?
-      render json: @habit, status: :ok
+    habit = @user.habits.find_by id: params[:id]
+    habit.update(habit_params)
+    if habit.valid?
+      render json: habit, status: :ok
     else 
-      render json: { errors: @habit.errors.full_messages },
+      render json: { errors: habit.errors.full_messages },
              status: :unprocessable_entity
     end
   end
