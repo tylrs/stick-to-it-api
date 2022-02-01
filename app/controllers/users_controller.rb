2e4e2e25b471973ef959752d837e9ee88e03aafb
Taylor Galloway <galloway.taylor@gmail.com>
2022-02-01 12:12:02 -0600

    Add skip authorization before user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authorize_request, except: :create
+  skip_before_action :authorize_request, only: [:create]
   before_action :find_user, except: %i[create index]
 
   # GET /users
