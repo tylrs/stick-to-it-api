0224c834336835532cb2fd4909586e5a631e8e78
Taylor Galloway <galloway.taylor@gmail.com>
2022-02-04 16:40:15 -0600

    Add habit logs routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root "application#hello"
   resources :users do 
-    resources :habits
+    resources :habits do
+      resources :habit_logs
+    end
   end
   post '/auth/login', to: 'authentication#login'
   get '/*a', to: 'application#not_found'
