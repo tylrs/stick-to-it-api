df38d9056acb3cb5ed99c5aae16b569da0b5c82d
Taylor Galloway <galloway.taylor@gmail.com>
2022-02-04 16:46:22 -0600

    Add method for retrieving a particular habit log

--- app/controllers/habit_logs_controller.rb
+++ app/controllers/habit_logs_controller.rb
@@ -1,4 +1,12 @@
 class HabitLogsController < ApplicationController
   def update
   end
+
+  private
+
+  def find_habit_log
+    @habit_log = HabitLog.find_by id: params[:habit_log_id]
+  rescue ActiveRecord::RecordNotFound
+    render json: { errors: 'User not found' }, status: :not_found
+  end
 end
