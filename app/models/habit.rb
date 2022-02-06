80a54c7bb3f2037fbf1121ec6bef9cf9ec9fbf48
Taylor Galloway <galloway.taylor@gmail.com>
2022-02-06 15:09:40 -0600

    Add destroy habit logs on habit deletion

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,6 +1,6 @@
 class Habit < ApplicationRecord
   belongs_to :user
-  has_many :habit_logs
+  has_many :habit_logs, dependent: :destroy
 
   validates :name, presence: true
   validates :description, presence: true
