7530759e968b1758ac80f39e3263e4fe87531c20
Taylor Galloway <galloway.taylor@gmail.com>
2022-02-06 14:21:36 -0600

    Generate habit logs when a habit is created

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -7,8 +7,19 @@ class HabitsController < ApplicationController
   end
 
   def create
-    habit = @user.habits.build(habit_params)
+    habit = @user.habits.build(name: habit_params[:name], description: habit_params[:description])
     if @user.habits << habit
+      date1 = Date.parse(habit_params[:start_datetime])
+      date2 = Date.parse(habit_params[:end_datetime])
+      num_logs = (date2 - date1).numerator
+      num_logs += 1
+      habit = @user.habits.order("created_at").last
+      current_date = date1
+      num_logs.times {
+        log = habit.habit_logs.build(scheduled_at: "#{current_date}")
+        habit.habit_logs << log
+        current_date += 1.day       
+      }
       render json: habit, status: :created
     else
       render json: { errors: habit.errors.full_messages },
@@ -46,7 +57,7 @@ class HabitsController < ApplicationController
 
   def habit_params
       params.permit(
-        :name, :description, :start_datetime
+        :name, :description, :user_id, :start_datetime, :end_datetime
       )
   end  
 end
