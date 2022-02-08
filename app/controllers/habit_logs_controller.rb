512c14488cf1a6d51764a748dd68231a2b1a19d4
Taylor Galloway <galloway.taylor@gmail.com>
2022-02-08 10:44:57 -0600

    Add update action and remove index action

--- app/controllers/habit_logs_controller.rb
+++ app/controllers/habit_logs_controller.rb
@@ -1,24 +1,20 @@
 class HabitLogsController < ApplicationController
-  before_action :find_habit
-
-  def index
-    habit_logs = @habit.habit_logs.all
-    render json: habit_logs, status: :ok  
-  end
+  before_action :find_habit_log
 
   def update
+    if @habit_log.completed_at
+      @habit_log.update(completed_at: nil)
+      render json: {message: "Habit Marked Incomplete"}
+    else
+      @habit_log.update(completed_at: @habit_log.scheduled_at)
+      render json: {message: "Habit Marked Complete"}
+    end
   end
 
   private
 
-  def find_habit 
-    @habit = Habit.find_by id: params[:habit_id]
-  rescue ActiveRecord::RecordNotFound
-    render json: { errors: 'Habit not found' }, status: :not_found
-  end
-
   def find_habit_log
-    @habit_log = HabitLog.find_by id: params[:habit_log_id]
+    @habit_log = HabitLog.find_by id: params[:id]
   rescue ActiveRecord::RecordNotFound
     render json: { errors: 'Habit Log Not Found' }, status: :not_found
   end
