a4a8e48c8e56dd74691b1bbe395883837ce03416
Taylor Galloway <galloway.taylor@gmail.com>
2022-02-08 15:52:20 -0600

    Extract habit creation logic into a service

--- app/services/habit_creation_service.rb
+++ app/services/habit_creation_service.rb
@@ -1,4 +1,15 @@
 module HabitCreationService 
-  def self.create
+  def self.create(habit_params, habit, user)
+    date1 = Date.parse(habit_params[:start_datetime])
+    date2 = Date.parse(habit_params[:end_datetime])
+    num_logs = (date2 - date1).numerator
+    num_logs += 1
+    habit = user.habits.order("created_at").last
+    current_date = date1
+    num_logs.times {
+      log = habit.habit_logs.build(scheduled_at: "#{current_date}")
+      habit.habit_logs << log
+      current_date += 1.day       
+    }
   end   
 end  
