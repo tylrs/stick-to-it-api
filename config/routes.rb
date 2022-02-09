6f7d5881b1e362546b78ba628dc12cd4eba35637
Taylor Galloway <galloway.taylor@gmail.com>
2022-02-09 15:19:59 -0600

    move route for today habits to top

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "application#hello"
+  get "/users/:user_id/habits/today", to: "habits#show_today"
   resources :users do 
     resources :habits do
       resources :habit_logs
