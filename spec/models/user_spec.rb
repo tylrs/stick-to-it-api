9284db38d3bb457dd5d17dbcffe93d9a8fa59252
Taylor Galloway <galloway.taylor@gmail.com>
2022-02-09 21:21:46 -0600

    Add basic user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # before(:all) do
+  #   @user = User.create(name: "Brenda Johnson", 
+  #                       username: "brenjo1", 
+  #                       email: "brendajohnson@example.com", 
+  #                       password: "123456", 
+  #                       password_confirmation: "123456")
+  # end
+
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:username) }
+    it { should validate_uniqueness_of(:username) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_presence_of(:password) }
+    it { should have_secure_password(:password) }
+  end
+
+  describe "relationships" do
+    it { should have_many(:habits) }
+  end
+
 end
