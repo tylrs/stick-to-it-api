541552c6a4be5ee2292c7eec879d87aa41f973cc
Taylor Galloway <galloway.taylor@gmail.com>
2022-02-09 21:17:43 -0600

    Add another validation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :habits
   # mount_uploader :avatar, AvatarUploader
+  validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :username, presence: true, uniqueness: true
