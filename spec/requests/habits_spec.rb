f23152a78dea94ceca86700796f953f68eb6a008
Taylor Galloway <galloway.taylor@gmail.com>
2022-02-10 14:05:26 -0600

    Add test for creating habits

--- spec/requests/habits_spec.rb
+++ spec/requests/habits_spec.rb
@@ -1,24 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe "Habits", type: :request do
-  # describe "GET /show" do
-    
-  #   end
-  # end
+  describe "Create" do
+    before(:all) do
+      @user = create(:user)
+    end
 
-  # describe "GET /create" do
-    
-  #   end
-  # end
+    it "Should create a habit with required info" do
+      habitInfo = {
+        name: "Running",
+        description: "Run every day",
+        start_datetime: "2022/02/10",
+        end_datetime: "2022/02/15"
+      }
+      token = JsonWebTokenService.encode(user_id: @user.id)
+      headers = {"Content-type": "application/json", "Authorization": "Bearer #{token}"}
+      post "/users/#{@user.id}/habits", headers: headers, params: JSON.generate(habitInfo)
 
-  # describe "GET /update" do
-    
-  #   end
-  # end
+      created_habit = Habit.last
+      habit_logs = HabitLog.where(habit_id: created_habit.id)
 
-  # describe "GET /destroy" do
-    
-  #   end
-  # end
-
+      expect(response.status).to eq 201
+      expect(created_habit.name).to eq "Running"
+      expect(habit_logs.count).to eq 6
+    end
+  end
 end
