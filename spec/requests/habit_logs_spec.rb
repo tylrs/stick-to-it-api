2819f208266bbb75ce574a169ae82882470d728e
Taylor Galloway <galloway.taylor@gmail.com>
2022-02-10 10:35:37 -0600

    Remove default tests

--- spec/requests/habit_logs_spec.rb
+++ spec/requests/habit_logs_spec.rb
@@ -3,8 +3,6 @@ require 'rails_helper'
 RSpec.describe "HabitLogs", type: :request do
   describe "GET /update" do
     it "returns http success" do
-      get "/habit_logs/update"
-      expect(response).to have_http_status(:success)
     end
   end
 
