8a8b19842fd4c764177c6de2c062cea099d1190c
Taylor Galloway <galloway.taylor@gmail.com>
2022-02-16 13:04:13 -0600

    Remove some logic from render method

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -3,12 +3,23 @@ class AuthenticationController < ApplicationController
 
   # POST /auth/login
   def login
-    @user = User.find_by_email(params[:email])
-    if @user&.authenticate(params[:password])
+    @user = User.find_by_email(login_params[:email])
+
+    if @user&.authenticate(login_params[:password])
       token = JsonWebTokenService.encode(user_id: @user.id)
-      time = Time.now + 24.hours.to_i
-      render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
-                     user: { id: @user.id, name: @user.name, username: @user.username, email: @user.email }}, status: :ok
+      expiration_time = (Time.now + 24.hours.to_i).strftime("%m-%d-%Y %H:%M")
+      user_payload = { 
+        id: @user.id, 
+        name: @user.name, 
+        username: @user.username, 
+        email: @user.email 
+      }
+
+      render json: { 
+        token: token, 
+        exp: expiration_time,          
+        user: user_payload
+      }, status: :ok
     else
       render json: { error: 'unauthorized' }, status: :unauthorized
     end
