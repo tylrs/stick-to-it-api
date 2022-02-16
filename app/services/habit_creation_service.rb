529edb8912c89325bcaa19ca9a87cd9e95081137
Taylor Galloway <galloway.taylor@gmail.com>
2022-02-16 13:05:10 -0600

    Change from numerator to integer method

--- app/services/habit_creation_service.rb
+++ app/services/habit_creation_service.rb
@@ -2,7 +2,7 @@ module HabitCreationService
   def self.create(habit_params, habit, user)
     date1 = Date.parse(habit_params[:start_datetime])
     date2 = Date.parse(habit_params[:end_datetime])
-    num_logs = (date2 - date1).numerator
+    num_logs = (date2 - date1).to_i
     num_logs += 1
     habit = user.habits.order("created_at").last
     current_date = date1
