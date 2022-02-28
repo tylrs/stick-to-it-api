2e392cc508c9015d03b7ad2c7b69c9d81e38d4c1
Taylor Galloway <galloway.taylor@gmail.com>
2022-02-28 16:52:32 -0600

    Change sample method

--- app/jobs/create_weekly_habit_logs_job.rb
+++ app/jobs/create_weekly_habit_logs_job.rb
@@ -1,7 +1,9 @@
 class CreateWeeklyHabitLogsJob < ApplicationJob
+  sidekiq_options queue: :default, retry: 3
   queue_as :default
 
-  def perform(*args)
-    
+  def perform(task)
+    puts "job is running"
   end
+
 end
