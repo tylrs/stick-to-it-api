b74081d1f6e789320df12439eb71a443526e48b1
Taylor Galloway <galloway.taylor@gmail.com>
2022-02-28 13:33:17 -0600

    Add sidekiq configuration

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,6 @@ module StickToItApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
   end
 end
