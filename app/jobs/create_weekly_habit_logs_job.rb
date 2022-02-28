e513a44f662cda34d8e0179ce3a81e788759ce2f
Taylor Galloway <galloway.taylor@gmail.com>
2022-02-28 16:58:46 -0600

    Remove arguments

--- app/jobs/create_weekly_habit_logs_job.rb
+++ app/jobs/create_weekly_habit_logs_job.rb
@@ -2,7 +2,7 @@ class CreateWeeklyHabitLogsJob < ApplicationJob
   sidekiq_options queue: :default, retry: 3
   queue_as :default
 
-  def perform(task)
+  def perform()
     puts "job is running"
   end
 
