ef00ad2408969060fc544fd78e1c31139b998644
Taylor Galloway <galloway.taylor@gmail.com>
2022-03-01 11:30:15 -0600

    Return to original current method

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,12 +1,9 @@
 class StatusController < ApplicationController
   def current
-    # current_sha = GithubService.get_current_sha
+    current_sha = GithubService.get_current_sha
 
-    # render json: {
-    #   message: current_sha
-    # }, status: 200
-
-    CreateWeeklyHabitLogsJob.perform_later
-    render json: {message: "Job is running"}, status: 200
+    render json: {
+      message: current_sha
+    }, status: 200
   end
 end
