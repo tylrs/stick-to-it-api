f4b115687c5450bf07a9be8545d8f9a508e561f5
Taylor Galloway <galloway.taylor@gmail.com>
2022-03-01 11:27:20 -0600

    Add temporary method for testing heroku config

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,9 +1,12 @@
 class StatusController < ApplicationController
   def current
-    current_sha = GithubService.get_current_sha
+    # current_sha = GithubService.get_current_sha
 
-    render json: {
-      message: current_sha
-    }, status: 200
+    # render json: {
+    #   message: current_sha
+    # }, status: 200
+
+    CreateWeeklyHabitLogsJob.perform_later
+    render json: {message: "Job is running"}, status: 200
   end
 end
