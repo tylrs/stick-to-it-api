49e8c398c02ec26ef22b9035afc87a6a1a0b6422
Taylor Galloway <galloway.taylor@gmail.com>
2022-03-01 17:16:45 -0600

    Rename service

--- app/services/habit_logs_creation_service.rb
+++ app/services/habit_logs_creation_service.rb
@@ -1,9 +1,9 @@
-module HabitCreationService 
+module HabitLogsCreationService 
   def self.create(habit_params, user)
     date1 = Date.parse(habit_params[:start_datetime])
     date2 = Date.parse(habit_params[:end_datetime])
     habit = user.habits.order("created_at").last
-    num_logs = HabitCreationService.get_num_logs(date1, date2)
+    num_logs = HabitLogsCreationService.get_num_logs(date1, date2)
     current_date = date1
     num_logs.times {
       log = habit.habit_logs.build(scheduled_at: "#{current_date}")
--- lib/tasks/schedule_habit_logs.rake
+++ lib/tasks/schedule_habit_logs.rake
@@ -1,4 +1,4 @@
-namespace :habit_logs_scheduler  do
+namespace :habit_logs_scheduler do
   desc "Schedule Week Habit Logs"
   task schedule_week_habit_logs: :environment do
     CreateWeeklyHabitLogsJob.perform_later
