4c32be0f84ee6807ba064116e5f8aae381b64df6
Taylor Galloway <galloway.taylor@gmail.com>
2022-03-02 16:10:51 -0600

    Add finding next sunday for habit logs job

--- app/jobs/create_weekly_habit_logs_job.rb
+++ app/jobs/create_weekly_habit_logs_job.rb
@@ -2,9 +2,8 @@ class CreateWeeklyHabitLogsJob < ApplicationJob
   sidekiq_options queue: :default, retry: 3
 
   def perform
-    date = Date.today
-    puts "This job ran on #{date}"
-    puts "This Job will be scheduled"
+    next_sunday = Date.next_occuring(:sunday)
+    
   end
 
 end
