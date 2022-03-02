1d5d4ad9e7e229699081f5e23e254b7b4ffe7648
Taylor Galloway <galloway.taylor@gmail.com>
2022-03-02 14:45:59 -0600

    Update logic for creating habit logs on habit creation

--- app/services/habit_logs_creation_service.rb
+++ app/services/habit_logs_creation_service.rb
@@ -2,30 +2,39 @@ module HabitLogsCreationService
   def self.create(habit_params, user)
     date1 = Date.parse(habit_params[:start_datetime])
     date2 = Date.parse(habit_params[:end_datetime])
+    today = Date.today
+    @next_saturday = today.end_of_week(start_day = :sunday)
+    if date1 <= @next_saturday
+      date_limit = HabitLogsCreationService.determine_date_limit(date1, date2)
+      num_logs = HabitLogsCreationService.get_num_logs(date1, date_limit)
+      HabitLogsCreationService.create_logs(num_logs, date1, user)
+    end
+    #Should I return something if the starting date is later than Saturday?
+  end
+  
+  def self.determine_date_limit(date1, date2)
+    @date_limit
+
+    if date1 < @next_saturday && date2 > @next_saturday
+      @date_limit = @next_saturday
+    elsif date2 < @next_saturday
+      @date_limit = date2
+    end
+    @date_limit
+  end
+  
+  def self.get_num_logs(date1, date_limit)
+    num_logs = (date_limit - date1).to_i
+    num_logs += 1
+  end
+
+  def self.create_logs(num_logs, date1, user)
     habit = user.habits.order("created_at").last
-    num_logs = HabitLogsCreationService.get_num_logs(date1, date2)
     current_date = date1
     num_logs.times {
       log = habit.habit_logs.build(scheduled_at: "#{current_date}")
       habit.habit_logs << log
       current_date += 1.day       
     }
-  end
-  
-  def self.create_initial_logs(date1, date2)
-    today = Date.today
-    next_saturday = today.end_of_week(start_day = :sunday)
-   if date1 > next_saturday
-     puts "create no logs because background job will take care of it"
-   elsif date2 < next_saturday
-     puts "create logs from date 1 to date 2 because date 2 is sooner than Saturday and we don't need a log for saturday"
-   else
-     puts "create logs from date1 till next saturday"
-   end
-  end  
-
-  def self.get_num_logs(date1, date2)
-    num_logs = (date2 - date1).to_i
-    num_logs += 1
   end
 end  
