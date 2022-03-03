2c4598f1c7b36e34becca1cced69a3ea3fd882d7
Taylor Galloway <galloway.taylor@gmail.com>
2022-03-03 12:52:04 -0600

    Add test for habit start date happening after current week

--- spec/requests/habits_spec.rb
+++ spec/requests/habits_spec.rb
@@ -51,5 +51,26 @@ RSpec.describe "Habits", type: :request do
       expect(habit_logs.first.scheduled_at.to_s).to eq "2022-02-10 00:00:00 UTC"
       expect(habit_logs.last.scheduled_at.to_s).to eq "2022-02-11 00:00:00 UTC"
     end
+
+    it "Should create a habit and no habit logs if the start date is after the current week's next Saturday" do
+      allow(Date).to receive(:today).and_return Date.new(2022,2,10)
+      habitInfo = {
+        name: "Running",
+        description: "Run every day",
+        start_datetime: "2022/02/13",
+        end_datetime: "2022/02/20"
+      }
+      token = JsonWebTokenService.encode(user_id: @user.id)
+      headers = {"Content-type": "application/json", "Authorization": "Bearer #{token}"}
+
+      post "/users/#{@user.id}/habits", headers: headers, params: JSON.generate(habitInfo)
+
+      created_habit = Habit.last
+      habit_logs = HabitLog.where(habit_id: created_habit.id)
+
+      expect(response.status).to eq 201
+      expect(created_habit.name).to eq "Running"
+      expect(habit_logs.count).to eq 0
+    end
   end
 end
