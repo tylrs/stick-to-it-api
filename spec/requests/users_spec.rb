5c19f8a8e5d5d7f3ce6e38b41ae67f57a17274db
Taylor Galloway <galloway.taylor@gmail.com>
2022-03-06 14:23:13 -0600

    Add sad path test for user creation

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -19,5 +19,23 @@ RSpec.describe "Users", type: :request do
       expect(JSON.parse(response.body)["name"]).to eq "John Bob"
       expect(created_user.name).to eq "John Bob"
     end
+
+    it "Should not be able to create a user without all required info" do
+      user_details = {
+        name: "",
+        username: "johnbob79",
+        email: "johnbob7@example.com",
+        password: "123456",
+        password_confirmation: ""
+      }
+
+      headers = {"CONTENT_TYPE"  => "application/json"}
+      post "/users", headers: headers, params: JSON.generate(user_details)
+      created_user = User.last
+      data = JSON.parse(response.body)
+      expect(response.status).to eq 422
+      expect(data["errors"][0]).to eq "Password confirmation doesn't match Password"
+      expect(data["errors"][1]).to eq "Name can't be blank"
+    end
   end
 end
