53d17b8806d3234f944d918c9420506397e08ab4
Taylor Galloway <galloway.taylor@gmail.com>
2022-03-08 12:59:21 -0600

    Fix validations

--- app/models/habit_plan.rb
+++ app/models/habit_plan.rb
@@ -3,6 +3,6 @@ class HabitPlan < ApplicationRecord
   belongs_to :habit
   has_many :habit_logs
 
-  validates :start_datetime, presence :true
-  validates :end_datetime, presence :true
+  validates :start_datetime, presence: :true
+  validates :end_datetime, presence: :true
 end
