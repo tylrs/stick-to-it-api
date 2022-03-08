eb795f62a0f4fb6b98da5622df89012bcc4cb12f
Taylor Galloway <galloway.taylor@gmail.com>
2022-03-08 17:30:59 -0600

    Add validation to habit log

--- app/models/habit_log.rb
+++ app/models/habit_log.rb
@@ -2,4 +2,6 @@ class HabitLog < ApplicationRecord
   belongs_to :habit_plan
   has_one :user, through: :habit_plan
   has_one :habit, through: :habit_plan
+
+  validates :scheduled_at, presence: true
 end
