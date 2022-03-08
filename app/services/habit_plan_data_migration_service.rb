010365090f41444cebcea68a12ce5db9d424b78d
Taylor Galloway <galloway.taylor@gmail.com>
2022-03-08 16:11:41 -0600

    Convert to bang method

--- app/services/habit_plan_data_migration_service.rb
+++ app/services/habit_plan_data_migration_service.rb
@@ -3,8 +3,8 @@ module HabitPlanDataMigrationService
     Habit.includes(:habit_logs).each do |habit|
       start_datetime = habit.habit_logs.first.scheduled_at
       end_datetime = habit.habit_logs.last.scheduled_at
-      habit_plan = HabitPlan.create(habit_id: habit.id, user_id: habit.user_id, start_datetime: start_datetime, end_datetime: end_datetime)
-      habit.habit_logs.update_all(habit_plan_id: habit_plan.id)
+      habit_plan = HabitPlan.create!(habit_id: habit.id, user_id: habit.user_id, start_datetime: start_datetime, end_datetime: end_datetime)
+      habit.habit_logs.update_all!(habit_plan_id: habit_plan.id)
     end
   end
 end
