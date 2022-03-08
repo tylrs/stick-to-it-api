0a39e74da49e4aaceab7543fba606e4cc0219b75
Taylor Galloway <galloway.taylor@gmail.com>
2022-03-08 16:16:56 -0600

    Fix update_all

--- app/services/habit_plan_data_migration_service.rb
+++ app/services/habit_plan_data_migration_service.rb
@@ -4,7 +4,7 @@ module HabitPlanDataMigrationService
       start_datetime = habit.habit_logs.first.scheduled_at
       end_datetime = habit.habit_logs.last.scheduled_at
       habit_plan = HabitPlan.create!(habit_id: habit.id, user_id: habit.user_id, start_datetime: start_datetime, end_datetime: end_datetime)
-      habit.habit_logs.update_all!(habit_plan_id: habit_plan.id)
+      habit.habit_logs.update_all(habit_plan_id: habit_plan.id)
     end
   end
 end
