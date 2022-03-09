46678124a8c3060db9e0e789b88a4ef81d6468f8
Taylor Galloway <galloway.taylor@gmail.com>
2022-03-09 09:05:45 -0600

    Update model tests

--- spec/models/habit_log_spec.rb
+++ spec/models/habit_log_spec.rb
@@ -1,7 +1,13 @@
 require "rails_helper"
 
 RSpec.describe HabitLog, type: :model do
-  describe "HabitLog Relationships" do
-    it { should belong_to(:habit) }
+  describe "Relationships" do
+    it { should belong_to(:habit_plan) }
+    it { should have_one(:user) }
+    it { should have_one(:habit) }
+  end
+
+  describe "Validations" do
+    it { should validate_presence_of(:scheduled_at) }
   end
 end
--- spec/models/habit_plan_spec.rb
+++ spec/models/habit_plan_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe HabitPlan, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Relationships" do
+    it { should belong_to(:user) }
+    it { should belong_to(:habit) }
+    it { should have_many(:habit_logs) }
+  end
+
+  describe "Validations" do
+    it { should validate_presence_of(:start_datetime) }
+    it { should validate_presence_of(:end_datetime) }
+  end
 end
