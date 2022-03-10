431884bbcb11e8994b53393770427e08de6849d6
Taylor Galloway <galloway.taylor@gmail.com>
2022-03-10 14:12:31 -0600

    Revert to v1 habits spec

--- spec/api/v1/requests/habits_spec.rb
+++ spec/api/v1/requests/habits_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe "Habits", type: :request do
       @headers = {"Content-type": "application/json", "Authorization": "Bearer #{token}"}
     end
 
-    it "Should create a habit, habit plan, and habit logs until the current week's next Saturday if the end date is after or equal to next Saturday" do
+    it "Should create a habit and habit logs until the current week's next Saturday if the end date is after or equal to next Saturday" do
       allow(Date).to receive(:today).and_return Date.new(2022,2,1)
       habitInfo = {
         name: "Running",
@@ -18,22 +18,18 @@ RSpec.describe "Habits", type: :request do
       }
 
       post "/users/#{@user.id}/habits", headers: @headers, params: JSON.generate(habitInfo)
+
       created_habit = Habit.last
-      created_habit_plan = HabitPlan.last
-      habit_logs = HabitLog.where(habit_plan_id: created_habit_plan.id)
-      
+      habit_logs = HabitLog.where(habit_id: created_habit.id)
+
       expect(response.status).to eq 201
       expect(created_habit.name).to eq "Running"
-      expect(created_habit_plan.user_id).to eq @user.id
-      expect(created_habit_plan.habit_id).to eq created_habit.id
-      expect(created_habit_plan.start_datetime).to eq "2022-02-02 00:00:00 UTC"
-      expect(created_habit_plan.end_datetime).to eq "2022-02-08 00:00:00 UTC"
       expect(habit_logs.count).to eq 4
       expect(habit_logs.first.scheduled_at.to_s).to eq "2022-02-02 00:00:00 UTC"
       expect(habit_logs.last.scheduled_at.to_s).to eq "2022-02-05 00:00:00 UTC"
     end
 
-    it "Should create a habit, habit plan, and habit logs until the end date if the end date is before the current week's next Saturday" do
+    it "Should create a habit and habit logs until the end date if the end date is before the current week's next Saturday" do
       allow(Date).to receive(:today).and_return Date.new(2022,2,1)
       habitInfo = {
         name: "Running",
@@ -45,21 +41,16 @@ RSpec.describe "Habits", type: :request do
       post "/users/#{@user.id}/habits", headers: @headers, params: JSON.generate(habitInfo)
 
       created_habit = Habit.last
-      created_habit_plan = HabitPlan.last
-      habit_logs = HabitLog.where(habit_plan_id: created_habit_plan.id)
-      
+      habit_logs = HabitLog.where(habit_id: created_habit.id)
+
       expect(response.status).to eq 201
       expect(created_habit.name).to eq "Running"
-      expect(created_habit_plan.user_id).to eq @user.id
-      expect(created_habit_plan.habit_id).to eq created_habit.id
-      expect(created_habit_plan.start_datetime).to eq "2022-02-02 00:00:00 UTC"
-      expect(created_habit_plan.end_datetime).to eq "2022-02-04 00:00:00 UTC"
       expect(habit_logs.count).to eq 3
       expect(habit_logs.first.scheduled_at.to_s).to eq "2022-02-02 00:00:00 UTC"
       expect(habit_logs.last.scheduled_at.to_s).to eq "2022-02-04 00:00:00 UTC"
     end
 
-    it "Should create a habit, habit plan, and no habit logs if the start date is after the current week's next Saturday" do
+    it "Should create a habit and no habit logs if the start date is after the current week's next Saturday" do
       allow(Date).to receive(:today).and_return Date.new(2022,2,1)
       habitInfo = {
         name: "Running",
@@ -71,15 +62,10 @@ RSpec.describe "Habits", type: :request do
       post "/users/#{@user.id}/habits", headers: @headers, params: JSON.generate(habitInfo)
 
       created_habit = Habit.last
-      created_habit_plan = HabitPlan.last
-      habit_logs = HabitLog.where(habit_plan_id: created_habit_plan.id)
+      habit_logs = HabitLog.where(habit_id: created_habit.id)
 
       expect(response.status).to eq 201
       expect(created_habit.name).to eq "Running"
-      expect(created_habit_plan.user_id).to eq @user.id
-      expect(created_habit_plan.habit_id).to eq created_habit.id
-      expect(created_habit_plan.start_datetime).to eq "2022-02-13 00:00:00 UTC"
-      expect(created_habit_plan.end_datetime).to eq "2022-02-20 00:00:00 UTC"
       expect(habit_logs.count).to eq 0
     end
 
