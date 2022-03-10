e1ccc7ff63a6fc37e4d182ff9886d20cfdf0df4e
Taylor Galloway <galloway.taylor@gmail.com>
2022-03-10 16:56:17 -0600

    Update HabitLog test

--- spec/api/v2/requests/habit_logs_spec.rb
+++ spec/api/v2/requests/habit_logs_spec.rb
@@ -5,25 +5,25 @@ RSpec.describe "HabitLogs", type: :request do
     before(:all) do
       @habit_log = create(:habit_log)
       @user = User.last
-      @habit = Habit.last
+      @habit_plan = HabitPlan.last
       @token = JsonWebTokenService.encode(user_id: @user.id)
     end
 
     it "Should be able to mark an incomplete habit log as completed" do
       headers = {"Content-type": "application/json", "Authorization": "Bearer #{@token}"}
-      patch "/api/v2/users/#{@user.id}/habits/#{@habit.id}/habit_logs/#{@habit_log.id}", headers: headers
+      patch "/api/v2/users/#{@user.id}/habit_plans/#{@habit_plan.id}/habit_logs/#{@habit_log.id}", headers: headers
 
-      updated_logs = HabitLog.where(habit_id: @habit.id)
+      updated_logs = HabitLog.where(habit_plan_id: @habit_plan.id)
 
       expect(updated_logs.last.completed_at).to eq "2022-02-02 00:00:00 UTC"
     end
 
     it "Should be able to mark a completed habit log as incomplete" do
       headers = {"Content-type": "application/json", "Authorization": "Bearer #{@token}"}
-      patch "/api/v2/users/#{@user.id}/habits/#{@habit.id}/habit_logs/#{@habit_log.id}", headers: headers
-      patch "/api/v2/users/#{@user.id}/habits/#{@habit.id}/habit_logs/#{@habit_log.id}", headers: headers
+      patch "/api/v2/users/#{@user.id}/habit_plans/#{@habit_plan.id}/habit_logs/#{@habit_log.id}", headers: headers
+      patch "/api/v2/users/#{@user.id}/habit_plans/#{@habit_plan.id}/habit_logs/#{@habit_log.id}", headers: headers
 
-      updated_logs = HabitLog.where(habit_id: @habit.id)
+      updated_logs = HabitLog.where(habit_plan_id: @habit_plan.id)
 
       expect(updated_logs.last.completed_at).to eq nil
     end
