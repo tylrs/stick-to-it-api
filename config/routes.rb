f013de0713265fc36045a9d28f89907cad651ca1
Taylor Galloway <galloway.taylor@gmail.com>
2022-03-10 14:01:48 -0600

    Add versioned routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,27 @@
 Rails.application.routes.draw do
   root "status#current"
-
-  resources :users do 
-    resources :habits do
-      get :today, on: :collection, to: "habits#show_today"
-      resources :habit_logs
+  namespace :api do
+    namespace :v1 do
+      resources :users do 
+        resources :habits do
+          get :today, on: :collection, to: "habits#show_today"
+          resources :habit_logs
+        end   
+      end
+      post "/auth/login", to: "authentication#login"
+      get "/*a", to: "application#not_found"     
+    end
+    namespace :v2 do
+      resources :users do 
+        resources :habit_plans do
+          get :week, on: :collection, to: "habit_plans#show_week"
+          get :today, on: :collection, to: "habit_plans#show_today"
+          resources :habit_logs
+        end
+        resources :habits 
+      end
+      post "/auth/login", to: "authentication#login"
+      get "/*a", to: "application#not_found"     
     end
   end
-
-  post "/auth/login", to: "authentication#login"
-  get "/*a", to: "application#not_found"
 end
