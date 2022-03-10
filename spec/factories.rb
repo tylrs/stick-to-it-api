2cb31c73f023a7ba7ea561d22d9adad17ab23c69
Taylor Galloway <galloway.taylor@gmail.com>
2022-03-10 16:56:02 -0600

    Revise factory bot for new models

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,30 +1,29 @@
 FactoryBot.define do
-  factory :habit_plan do
-    start_date { "2022-02-03 11:07:31" }
-    end_date { "2022-02-10 11:07:31" }
-    user { nil }
-    habit { nil }
-  end
-
-  factory :user do
-    name {"Jim Bob"}
-    username {"jimbob79"}
-    email {"jimbob7@example.com"}
-    password {"123456"}
-    password_confirmation {"123456"}
-  end
-  
-  factory :habit_log do
-    scheduled_at {"2022/02/02"}
-    completed_at {nil}
-    habit
+  factory :user, aliases: [:creator] do
+    name { "Jim Bob" }
+    username { "jimbob79" }
+    email { "jimbob7@example.com" }
+    password { "123456" }
+    password_confirmation { "123456" }
   end
 
   factory :habit do
-    name {"Running"}
-    description {"Run every day"}
-    user
+    name { "Running" }
+    description { "Run every day" }
+    creator
+    habit_plans { association :habit_plan, habit: instance, user: creator }
   end
 
-
+  factory :habit_plan do
+    start_datetime { "2022-02-02 11:07:31" }
+    end_datetime { "2022-02-10 11:07:31" }
+    user
+    habit { association :habit, habit_plans: [instance], creator: user }
+  end
+  
+  factory :habit_log do
+    scheduled_at { "2022/02/02" }
+    completed_at { nil }
+    habit_plan
+  end
 end
