d7d2d2b8bed37d1fd8e4e8aeb2b907b865f36d18
Taylor Galloway <galloway.taylor@gmail.com>
2022-03-12 11:53:47 -0600

    Add first habit_plan spec

--- spec/api/v2/requests/habit_plans_spec.rb
+++ spec/api/v2/requests/habit_plans_spec.rb
@@ -6,23 +6,44 @@ RSpec.describe "HabitPlans", type: :request do
       @habit_log = create(:habit_log)
       @user = User.last
       @habit_plan = HabitPlan.last
-      @token = JsonWebTokenService.encode(user_id: @user.id)
+      token = JsonWebTokenService.encode(user_id: @user.id)
+      @headers = {"Content-type": "application/json", "Authorization": "Bearer #{token}"}
       FactoryBot.create_list(:habit_log, 3, habit_plan_id: @habit_plan.id) do |habit_log, i|
         date = Date.new(2022,2,3)
         date += i.days
         habit_log.scheduled_at = date
         habit_log.save
       end
-      debugger
     end
 
-    it "Should return HabitPlans for this week" do
+    it "Should return HabitPlans, habit info, and HabitLogs for this week and not any other week" do
       allow(Date).to receive(:today).and_return Date.new(2022,2,1)
+
+      get "/api/v2/users/#{@user.id}/habit_plans"
+
+      habits = JSON.parse(response)
+      habit1 = habits[0]
+
+      expect(response.status).to eq 201
+      expect(habits.length).to eq 1
+      expect(habit1["user_id"]).to eq @user.id
+      expect(habit1["habit_id"]).to eq @habit_plan.habit_id
+      expect(habit1["habit_plan_id"]).to eq @habit_plan.id
+      expect(habit1["name"]).to eq "Running"
+      expect(habit1["description"]).to eq "Run every day"
+      expect(habit1["start_datetime"]).to eq Date.new("2022/02/02")
+      expect(habit1["end_datetime"]).to eq Date.new("2022/02/10")
+      expect(habit1["habit_logs"].length).to eq 4
+      
+    end
+
+    it "Should not return HabitPlans for this week if there are none scheduled" do
+      allow(Date).to receive(:today).and_return Date.new(2022,2,8)
       
     end
 
     it "Should return HabitPlans for today" do
-      allow(Date).to receive(:today).and_return Date.new(2022,2,1)
+      allow(Date).to receive(:today).and_return Date.new(2022,2,2)
       
     end
   end
