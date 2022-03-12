9d3013322507a243e655407835502273b04d2946
Taylor Galloway <galloway.taylor@gmail.com>
2022-03-12 13:50:21 -0600

    Add service for getting HabitPlans

---
+++ app/services/habit_plans_filter_service.rb
@@ -0,0 +1,15 @@
+module HabitPlansFilterService
+  def self.get_week_plans(user_id)
+    week_start = Date.today
+    if !week_start.sunday?
+      week_start = week_start.beginning_of_week(start_day = :sunday)
+    end
+    habit_plans = HabitPlan.includes(:habit, :habit_logs)
+                      .where(user_id: user_id, habit_logs:{scheduled_at: week_start..(week_start + 6.days)})
+  end
+
+  def self.get_today_plans(user_id)
+    habits = Habit.includes(:habit_logs)
+                  .where(user_id: user_id, habit_logs:{scheduled_at: Date.today})
+  end
+end
