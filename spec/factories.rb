3492b7151ca86574fb71ff93dac8043d8da36561
Taylor Galloway <galloway.taylor@gmail.com>
2022-03-12 11:53:35 -0600

    Fix factory datetime

--- spec/factories.rb
+++ spec/factories.rb
@@ -15,8 +15,8 @@ FactoryBot.define do
   end
 
   factory :habit_plan do
-    start_datetime { "2022-02-02 11:07:31" }
-    end_datetime { "2022-02-10 11:07:31" }
+    start_datetime { "2022-02-02 00:00:00" }
+    end_datetime { "2022-02-10 00:00:00" }
     user
     habit { association :habit, habit_plans: [instance], creator: user }
   end
