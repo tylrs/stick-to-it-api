6c99da2f9f3f485ce081dfabec9c33e62fdf418c
Taylor Galloway <galloway.taylor@gmail.com>
2022-03-13 18:09:27 -0500

    Add dependent destroy to HabitPlan

--- app/models/habit_plan.rb
+++ app/models/habit_plan.rb
@@ -1,7 +1,7 @@
 class HabitPlan < ApplicationRecord
   belongs_to :user
   belongs_to :habit
-  has_many :habit_logs
+  has_many :habit_logs, dependent: :destroy
 
   validates :start_datetime, presence: true
   validates :end_datetime, presence: true
