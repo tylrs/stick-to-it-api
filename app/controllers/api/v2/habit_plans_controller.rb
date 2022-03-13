1389485918f3c866534b54f172c09a00ee3519c7
Taylor Galloway <galloway.taylor@gmail.com>
2022-03-13 17:09:14 -0500

    Fix rendering json

--- app/controllers/api/v2/habit_plans_controller.rb
+++ app/controllers/api/v2/habit_plans_controller.rb
@@ -2,7 +2,7 @@ class Api::V2::HabitPlansController < ApplicationController
 
   def show_week
     week_habit_plans = HabitPlansFilterService.get_week_plans(params[:user_id])
-    render json: week_habit_plans
+    render json: week_habit_plans,
            include: [habit: {only: [:name, :description]}, habit_logs: {only: [:id, :habit_id, :scheduled_at, :completed_at]}], 
            status: :ok
   end
