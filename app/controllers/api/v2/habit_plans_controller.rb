d47d61981816086246de0707ecc3451014f4ca88
Taylor Galloway <galloway.taylor@gmail.com>
2022-03-13 17:14:03 -0500

    Add today controller

--- app/controllers/api/v2/habit_plans_controller.rb
+++ app/controllers/api/v2/habit_plans_controller.rb
@@ -8,6 +8,10 @@ class Api::V2::HabitPlansController < ApplicationController
   end
 
   def show_today
+    today_habit_plans = HabitPlansFilterService.get_today_plans(params[:user_id])
+    render json: today_habit_plans,
+           include: [habit: {only: [:name, :description]}, habit_logs: {only: [:id, :habit_id, :scheduled_at, :completed_at]}], 
+           status: :ok
   end
 
 end
--- app/services/habit_plans_filter_service.rb
+++ app/services/habit_plans_filter_service.rb
@@ -9,7 +9,7 @@ module HabitPlansFilterService
   end
 
   def self.get_today_plans(user_id)
-    habits = Habit.includes(:habit_logs)
+    habits = HabitPlan.includes(:habit, :habit_logs)
                   .where(user_id: user_id, habit_logs:{scheduled_at: Date.today})
   end
 end
