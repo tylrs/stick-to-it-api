c48953d8fee33b15e8c62cf7bb373f5567ce7c54
Taylor Galloway <galloway.taylor@gmail.com>
2022-03-14 13:06:54 -0500

    Change filter service for v1

--- app/services/habit_logs_filter_service.rb
+++ app/services/habit_logs_filter_service.rb
@@ -1,11 +1,12 @@
 module HabitLogsFilterService
+  #v1 
   def self.get_week_logs(user_id)
     week_start = Date.today
     if !week_start.sunday?
       week_start = week_start.beginning_of_week(start_day = :sunday)
     end
-    habits = Habit.includes(:habit_logs)
-                  .where(user_id: user_id, habit_logs:{scheduled_at: week_start..(week_start + 6.days)})
+    habit_plans = HabitPlan.includes(:habit, :habit_logs)
+                           .where(user_id: user_id, habit_logs:{scheduled_at: week_start..(week_start + 6.days)})
   end
 
   def self.get_today_logs(user_id)
