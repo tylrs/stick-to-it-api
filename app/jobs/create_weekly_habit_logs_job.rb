3d8216ce445d5f6ac11d7a5dfed36dea88f75ba7
Taylor Galloway <galloway.taylor@gmail.com>
2022-03-15 11:33:50 -0500

    Add set up for creating habit logs

--- app/jobs/create_weekly_habit_logs_job.rb
+++ app/jobs/create_weekly_habit_logs_job.rb
@@ -2,8 +2,23 @@ class CreateWeeklyHabitLogsJob < ApplicationJob
   sidekiq_options queue: :default, retry: 3
 
   def perform
-    next_sunday = Date.next_occuring(:sunday)
-    
+    # iterate through each and generate habit logs based on the date range 
+    next_sunday = Date.today.next_occurring(:sunday)
+    following_saturday = next_sunday.next_occurring(:saturday)
+    habit_plans = HabitPlan.where(start_datetime: ..following_saturday, end_datetime: next_sunday..)
+    habit_plans.each do |habit_plan|
+      range_beginning = habit_plan.start_datetime.to_datetime
+      range_end = habit_plan.end_datetime.to_datetime
+      if range_beginning < next_sunday
+        range_beginning = next_sunday
+      end
+      if range_end > following_saturday
+        range_end = following_saturday
+      end
+      num_logs = HabitLogsCreationService.get_num_logs(range_beginning, range_end)
+      puts "Num logs>>>>"
+      puts num_logs
+    end
   end
 
 end
