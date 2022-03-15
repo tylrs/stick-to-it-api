a9d23207476af078dd6a35b7a858bcc2f90719a1
Taylor Galloway <galloway.taylor@gmail.com>
2022-03-15 14:14:54 -0500

    Abstract out habit plan filtering into service

--- app/jobs/create_weekly_habit_logs_job.rb
+++ app/jobs/create_weekly_habit_logs_job.rb
@@ -2,18 +2,10 @@ class CreateWeeklyHabitLogsJob < ApplicationJob
   sidekiq_options queue: :default, retry: 3
 
   def perform
-    # habit plans filter service . get_new_week_habit_plans
-    # determine habit plans num logs
     habit_plans = HabitPlansFilterService.get_next_week_plans
     habit_plans.each do |habit_plan|
-      range_beginning = habit_plan.start_datetime.to_datetime
-      range_end = habit_plan.end_datetime.to_datetime
-      if range_beginning < next_sunday
-        range_beginning = next_sunday
-      end
-      if range_end > following_saturday
-        range_end = following_saturday
-      end
+      range_beginning, range_end = HabitPlansFilterService.determine_next_week_range(habit_plan)
+                                                           .values_at(:range_beginning, :range_end)
       num_logs = HabitLogsCreationService.get_num_logs(range_beginning, range_end)
       HabitLogsCreationService.create_logs(num_logs, range_beginning, habit_plan)
     end
--- app/services/habit_plans_filter_service.rb
+++ app/services/habit_plans_filter_service.rb
@@ -18,4 +18,18 @@ module HabitPlansFilterService
     habits = HabitPlan.includes(:habit, :habit_logs)
                       .where(user_id: user_id, habit_logs:{scheduled_at: Date.today})
   end
+
+  def self.determine_next_week_range(habit_plan)
+    next_sunday = Date.today.next_occurring(:sunday)
+    following_saturday = next_sunday.next_occurring(:saturday)
+    range_beginning = habit_plan.start_datetime.to_datetime
+    range_end = habit_plan.end_datetime.to_datetime
+    if range_beginning < next_sunday
+      range_beginning = next_sunday
+    end
+    if range_end > following_saturday
+      range_end = following_saturday
+    end
+    ranges = {range_beginning: range_beginning, range_end: range_end}
+  end
 end
