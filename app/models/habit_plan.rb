64a3650320a3f4568fd7afdb86079e3036c23d3c
Taylor Galloway <galloway.taylor@gmail.com>
2022-03-17 10:54:32 -0500

    Add HabitPlan scope

--- app/models/habit_plan.rb
+++ app/models/habit_plan.rb
@@ -5,4 +5,8 @@ class HabitPlan < ApplicationRecord
 
   validates :start_datetime, presence: true
   validates :end_datetime, presence: true
+   
+  scope :current_week, lambda {where("start_datetime <= ? AND end_datetime >= ?", 
+                                          Date.today.next_occurring(:saturday), 
+                                          Date.today.beginning_of_week(:sunday))}
 end
