4c1d7ee3f48f8f635bdbb84092107553bcff6b6d
Taylor Galloway <galloway.taylor@gmail.com>
2022-03-29 16:19:46 -0500

    Switch to lower case

--- spec/services/habit_log_update_service_spec.rb
+++ spec/services/habit_log_update_service_spec.rb
@@ -1,16 +1,16 @@
 require "rails_helper"
 
 RSpec.describe HabitLogUpdateService do
-  describe "Update Log" do
+  describe "update log" do
     
-    it "Should be able to mark an incomplete habit log as completed" do
+    it "should be able to mark an incomplete habit log as completed" do
       habit_log = create(:habit_log)
       updated_habit_log = HabitLogUpdateService.update(habit_log)
 
       expect(updated_habit_log.completed_at).to eq habit_log.scheduled_at
     end
 
-    it "Should be able to mark a compeleted habit log as incomplete" do
+    it "should be able to mark a compeleted habit log as incomplete" do
       habit_log = create(:habit_log)
       updated_habit_log = HabitLogUpdateService.update(habit_log)
       updated_habit_log = HabitLogUpdateService.update(updated_habit_log)
