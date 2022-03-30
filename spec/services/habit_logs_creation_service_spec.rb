5d45327c968a55b75e049d6161e336ef2371c168
Taylor Galloway <galloway.taylor@gmail.com>
2022-03-30 10:22:02 -0500

    Add tests for determining date limit on initial creation

--- spec/services/habit_logs_creation_service_spec.rb
+++ spec/services/habit_logs_creation_service_spec.rb
@@ -35,8 +35,36 @@ RSpec.describe HabitLogsCreationService do
       expect(@logs.first.scheduled_at).to eq(date1)
     end
 
-    it "schedules a habit log for 3 days past the date limit" do
+    it "schedules a habit log for specified number of days past the date limit" do
       expect(@logs.last.scheduled_at).to eq(date1 + 2.days)
+    end
+  end
+
+  describe "determine_date_limit_initial_creation" do
+    next_saturday = Date.new(2022,02,01).end_of_week(:sunday)
+
+    it "sets date limit to nil if date1 is after next_saturday" do
+      date1 = Date.new(2022,02,20)
+      date2 = Date.new(2022,02,22)
+      date_limit = HabitLogsCreationService.determine_date_limit_initial_creation(date1, date2, next_saturday)
+
+      expect(date_limit).to eq nil
+    end
+    
+    it "sets date limit to date2 if date2 is sooner than next Saturday" do
+      date1 = Date.new(2022,02,01)
+      date2 = Date.new(2022,02,04)
+      date_limit = HabitLogsCreationService.determine_date_limit_initial_creation(date1, date2, next_saturday)
+
+      expect(date_limit).to eq date2
+    end
+
+    it "sets date limit to next Saturday if date2 is greater than or equal to next Saturday" do
+      date1 = Date.new(2022,02,02)
+      date2 = Date.new(2022,02,21)
+      date_limit = HabitLogsCreationService.determine_date_limit_initial_creation(date1, date2, next_saturday)
+      
+      expect(date_limit).to eq next_saturday
     end
   end
 end
