10fef623d314e8206889ad130df2fd7113844954
Taylor Galloway <galloway.taylor@gmail.com>
2022-03-30 10:01:16 -0500

    Remove instance variable

--- app/services/habit_logs_creation_service.rb
+++ app/services/habit_logs_creation_service.rb
@@ -3,18 +3,18 @@ module HabitLogsCreationService
     date1 = Date.parse(habit_params[:start_datetime])
     date2 = Date.parse(habit_params[:end_datetime])
     today = Date.today  
-    @next_saturday = today.end_of_week(:sunday)
+    next_saturday = today.end_of_week(:sunday)
     habit_plan = user.habit_plans.order("created_at").last
-    if date1 <= @next_saturday
-      self.create_current_week_logs(date1, date2, habit_plan)
+    if date1 <= next_saturday
+      self.create_current_week_logs(date1, date2, habit_plan, next_saturday)
     end
-    if today == @next_saturday && date1 <= @next_saturday.next_occurring(:saturday) 
+    if today == next_saturday && date1 <= next_saturday.next_occurring(:saturday) 
       self.create_next_week_logs(habit_plan)                                                 
     end
   end
 
-  def self.create_current_week_logs(date1, date2, habit_plan)
-    date_limit = self.determine_date_limit_initial_creation(date1, date2)
+  def self.create_current_week_logs(date1, date2, habit_plan, next_saturday)
+    date_limit = self.determine_date_limit_initial_creation(date1, date2, next_saturday)
     num_logs = self.get_num_logs(date1, date_limit)
     self.create_logs(num_logs, date1, habit_plan)
   end
@@ -26,12 +26,11 @@ module HabitLogsCreationService
     self.create_logs(num_logs, range_beginning, habit_plan) 
   end
   
-  def self.determine_date_limit_initial_creation(date1, date2)
-    date_limit
-    if date1 <= @next_saturday && date2 >= @next_saturday
-      date_limit = @next_saturday
-    elsif date2 < @next_saturday
-      date_limit = date2
+  def self.determine_date_limit_initial_creation(date1, date2, next_saturday)
+    date_limit = if date1 <= next_saturday && date2 >= next_saturday
+      next_saturday
+    elsif date2 < next_saturday
+      date2
     end
     date_limit
   end
