10673bf32b31fdcc26c58bc519d908cb9af26b6d
Taylor Galloway <galloway.taylor@gmail.com>
2022-04-01 14:25:44 -0500

    Change expectation to response instead of DB change

--- spec/api/v2/requests/habit_logs_spec.rb
+++ spec/api/v2/requests/habit_logs_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "HabitLogs v2", type: :request do
-  describe "update habit logs" do
+  describe ".update" do
     let(:habit_log) {create(:habit_log)}
     let(:habit_plan) {habit_log.habit_plan}
     let(:user) {habit_log.habit_plan.user}
@@ -12,18 +12,28 @@ RSpec.describe "HabitLogs v2", type: :request do
       patch "/api/v2/users/#{user.id}/habit_plans/#{habit_plan.id}/habit_logs/#{habit_log.id}", headers: headers
     end
 
-    it "should be able to mark an incomplete habit log as completed" do
-      updated_logs = HabitLog.where(habit_plan_id: habit_plan.id)
+    context "when a habit log is incomplete to start" do
+      it "should respond with a success status" do
+        expect(response.status).to eq 200
+      end
 
-      expect(updated_logs.last.completed_at).to eq "2022-02-02 00:00:00 UTC"
+      it "should respond with an updated habit log" do
+        expect(json["habit_log"]["completed_at"]).not_to be_empty
+      end
     end
 
-    it "should be able to mark a completed habit log as incomplete" do
-      patch "/api/v2/users/#{user.id}/habit_plans/#{habit_plan.id}/habit_logs/#{habit_log.id}", headers: headers
+    context "when a habit log is marked completed to start" do
+      before do
+        patch "/api/v2/users/#{user.id}/habit_plans/#{habit_plan.id}/habit_logs/#{habit_log.id}", headers: headers
+      end
 
-      updated_logs = HabitLog.where(habit_plan_id: habit_plan.id)
+      it "should respond with a success status" do
+        expect(response.status).to eq 200
+      end
 
-      expect(updated_logs.last.completed_at).to eq nil
+      it "should be able to mark a completed habit log as incomplete" do
+        expect(json["habit_log"]["completed_at"]).to be_nil
+      end
     end
   end
 end
