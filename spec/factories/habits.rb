83f7669d0de05073e844304dc61a1d36b059b28d
Taylor Galloway <galloway.taylor@gmail.com>
2022-04-01 11:59:04 -0500

    Use Faker when creating habit name

--- spec/factories/habits.rb
+++ spec/factories/habits.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :habit do
-    name { "Running" }
-    description { "Run every day" }
+    name { Faker::Verb.ing_form }
+    description { "Every day" }
     creator
     habit_plans { association :habit_plan, habit: instance, user: creator }
   end
