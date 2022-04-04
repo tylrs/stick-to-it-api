5cabb77f789ae9a412c15f48a7d9ad2c9df3729b
Taylor Galloway <galloway.taylor@gmail.com>
2022-04-04 13:28:07 -0500

    Move related methods closer together

--- app/services/habit_plans_filter_service.rb
+++ app/services/habit_plans_filter_service.rb
@@ -7,16 +7,16 @@ module HabitPlansFilterService
              .where(habit_id: [habit_ids])
   end
 
-  def self.get_next_week_plans
-    next_sunday = Date.today.next_occurring(:sunday)
-    following_saturday = next_sunday.next_occurring(:saturday)
-    HabitPlan.where(start_datetime: ..following_saturday, end_datetime: next_sunday..)
-  end
-
   def self.get_today_and_partner_plans(user_id)
     habit_ids = Habit.includes(:habit_plans).where(habit_plans:{user_id: user_id}).ids
     habits = HabitPlan.includes(:user, :habit, :habit_logs)
                       .where(habit_id: [habit_ids], habit_logs:{scheduled_at: Date.today})
+  end
+
+  def self.get_next_week_plans
+    next_sunday = Date.today.next_occurring(:sunday)
+    following_saturday = next_sunday.next_occurring(:saturday)
+    HabitPlan.where(start_datetime: ..following_saturday, end_datetime: next_sunday..)
   end
 
   def self.determine_next_week_range(habit_plan)
