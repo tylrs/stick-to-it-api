b53c2ec46e481099aad5324a50e501e7e5d60acb
Taylor Galloway <galloway.taylor@gmail.com>
2022-04-07 16:07:29 -0400

    Remove extra line

--- app/services/habit_plans_filter_service.rb
+++ app/services/habit_plans_filter_service.rb
@@ -1,5 +1,4 @@
 module HabitPlansFilterService
-
   def self.get_week_and_partner_plans(user_id)
     habit_ids = Habit.includes(:habit_plans).where(habit_plans:{user_id: user_id}).ids
     HabitPlan.with_current_week_logs
