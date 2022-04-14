3f64cd03b5dd3fc1d9363a5a32e1a4ff2c36f2c5
Taylor Galloway <galloway.taylor@gmail.com>
2022-04-14 13:01:12 -0500

    Update background job

--- app/jobs/create_weekly_habit_logs_job.rb
+++ app/jobs/create_weekly_habit_logs_job.rb
@@ -4,9 +4,7 @@ class CreateWeeklyHabitLogsJob < ApplicationJob
   def perform
     habit_plans = HabitPlansFilterService.get_next_week_plans
     habit_plans.each do |habit_plan|
-      range_beginning, range_end = HabitPlansFilterService.determine_next_week_range(habit_plan)
-                                                          .values_at(:range_beginning, :range_end)
-      HabitLogsCreationService.create_logs(range_beginning..range_end, habit_plan)
+      HabitLogsCreationService.create_next_week_logs(habit_plan)
     end
   end
 end
