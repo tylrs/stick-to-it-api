4200e73dbc267dd859d73941de9c0feb9669431c
Taylor Galloway <galloway.taylor@gmail.com>
2022-04-14 12:38:01 -0500

    Finish refactoring determine date range

--- app/services/habit_logs_creation_service.rb
+++ app/services/habit_logs_creation_service.rb
@@ -11,14 +11,13 @@ module HabitLogsCreationService
   end
 
   def self.create_current_week_logs(plan_start, plan_end, habit_plan, next_saturday)
-    date_limit = determine_date_limit_initial_creation(plan_start, plan_end, next_saturday)
-    create_logs(plan_start..date_limit, habit_plan)
+    range = determine_date_range(habit_plan, "current_week")
+    create_logs(range, habit_plan) 
   end
 
   def self.create_next_week_logs(habit_plan)
-    range_beginning, range_end = HabitPlansFilterService.determine_next_week_range(habit_plan)
-                                                        .values_at(:range_beginning, :range_end)
-    create_logs(range_beginning..range_end, habit_plan) 
+    range = determine_date_range(habit_plan, "next_week")
+    create_logs(range, habit_plan) 
   end
   
   def self.determine_date_limit_initial_creation(plan_start, plan_end, next_saturday)
@@ -30,41 +29,41 @@ module HabitLogsCreationService
   def self.determine_date_range(habit_plan, type)
     plan_start = habit_plan.start_datetime.to_datetime
     plan_end = habit_plan.end_datetime.to_datetime
-    current_sunday = Date.today.beginning_of_week(:sunday)
-    current_saturday = Date.today.end_of_week(:sunday)
-    next_sunday = Date.today.next_occurring(:sunday)
-    next_saturday = next_sunday.next_occurring(:saturday)
-
-    return if plan_start > current_saturday && type == "current_week"
-    return if plan_end < current_sunday && type == "current_week"
-    return if plan_start > next_saturday && type == "next_week"
-    return if plan_end < next_sunday && type == "next_week"
-
+ 
     if type == "current_week"
-      range_start = current_sunday
-      range_end = current_saturday
+      week_start = Date.today.beginning_of_week(:sunday)
+      week_end = Date.today.end_of_week(:sunday)
     else
-      range_start = next_sunday
-      range_end = next_saturday
+      week_start = Date.today.next_occurring(:sunday)
+      week_end = week_start.end_of_week(:sunday)
     end
 
-    if plan_start >= current_sunday && type == "current_week"
-      range_start = plan_start 
-    end
+    range_start = determine_range_start(plan_start, week_start, week_end)
+    range_end = determine_range_end(plan_end, week_start, week_end)
 
-    if plan_end < current_saturday && type == "current_week"
-      range_end = plan_end
-    end
-
-    if plan_start > next_sunday && type == "next_week"
-      range_start = plan_start
-    end
-
-    if plan_end < next_saturday && type == "next_week"
-      range_end = plan_end
-    end
+    return if !range_start || !range_end
 
     range_start..range_end
+  end
+
+  def self.determine_range_start(plan_start, week_start, week_end)
+    return if plan_start > week_end
+
+    if plan_start > week_start
+      plan_start
+    else
+      week_start
+    end
+  end
+
+  def self.determine_range_end(plan_end, week_start, week_end)
+    return if plan_end < week_start
+
+    if plan_end < week_end
+      plan_end
+    else
+      week_end
+    end
   end
 
   def self.create_logs(date_range, habit_plan)
