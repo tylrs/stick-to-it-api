e4ecaf7ad78080fa182926174f62cadaad571715
Taylor Galloway <galloway.taylor@gmail.com>
2022-04-15 12:58:09 -0500

    Add get_week_end method

--- app/services/dates_in_week_service.rb
+++ app/services/dates_in_week_service.rb
@@ -6,9 +6,14 @@ module DatesInWeekService
     when "next_week"
       Date.today.next_occurring(:sunday)
     end
-    # week_start = Date.today.beginning_of_week(:sunday)
-    # week_end = Date.today.end_of_week(:sunday)
-    # week_start = Date.today.next_occurring(:sunday)
-    # week_end = week_start.end_of_week(:sunday)
+  end
+
+  def self.get_week_end(week_type)
+    case week_type
+    when "current_week"
+      Date.today.end_of_week(:sunday)
+    when "next_week"
+      Date.today.next_occurring(:sunday).end_of_week(:sunday)
+    end
   end
 end
--- spec/services/dates_in_week_service.rb
+++ spec/services/dates_in_week_service.rb
@@ -24,15 +24,23 @@ RSpec.describe DatesInWeekService do
   end
 
   describe ".get_week_end" do
+    before do
+      allow(Date).to receive(:today).and_return(Date.new(2022, 2, 2))
+    end
+    
     context "when week_type is current_week" do
       it "should return the next Saturday of the current week" do
-        
+        week_end = DatesInWeekService.get_week_end("current_week")
+
+        expect(week_end).to eq Date.new(2022, 2, 5)
       end
     end
 
     context "when week_type is next_week" do
       it "should return the next Saturday of next week" do
-        
+        week_end = DatesInWeekService.get_week_end("next_week")
+
+        expect(week_end).to eq Date.new(2022, 2, 12)
       end
     end
   end
