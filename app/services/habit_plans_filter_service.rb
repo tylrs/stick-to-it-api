877595c2a89b98112f2e4a91bd662e679549f078
Taylor Galloway <galloway.taylor@gmail.com>
2022-05-03 11:18:46 -0500

    Remove useless variable assignment

--- app/services/habit_plans_filter_service.rb
+++ app/services/habit_plans_filter_service.rb
@@ -8,8 +8,8 @@ module HabitPlansFilterService
 
   def self.get_today_and_partner_plans(user_id)
     habit_ids = Habit.includes(:habit_plans).where(habit_plans: { user_id: user_id }).ids
-    habits = HabitPlan.includes(:user, :habit, :habit_logs)
-                      .where(habit_id: [habit_ids], habit_logs: { scheduled_at: Date.today })
+    HabitPlan.includes(:user, :habit, :habit_logs)
+             .where(habit_id: [habit_ids], habit_logs: { scheduled_at: Date.today })
   end
 
   def self.get_next_week_plans
