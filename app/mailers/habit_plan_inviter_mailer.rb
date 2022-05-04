ae6aa11102b90c23db1f06eccf58b25dc62dbee4
Taylor Galloway <galloway.taylor@gmail.com>
2022-05-04 16:18:21 -0500

    Generate HabitPlanInviter

---
+++ app/mailers/habit_plan_inviter_mailer.rb
@@ -0,0 +1,2 @@
+class HabitPlanInviterMailer < ApplicationMailer
+end
---
+++ spec/mailers/previews/habit_plan_inviter_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/habit_plan_inviter
+class HabitPlanInviterPreview < ActionMailer::Preview
+
+end
