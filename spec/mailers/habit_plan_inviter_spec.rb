c813fa2131836663654294019db38c9df747fa82
Taylor Galloway <galloway.taylor@gmail.com>
2022-05-04 17:13:10 -0500

    Change method name

--- spec/mailers/habit_plan_inviter_spec.rb
+++ spec/mailers/habit_plan_inviter_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe HabitPlanInviterMailer, type: :mailer do
   describe ".habit_plan_invite_email" do
-    let(:email) {described_class.habit_plan_invite_email(user, habit_plan.id, "friend@example.com")}
+    let(:email) {described_class.invite_email(user, habit_plan.id, "friend@example.com")}
     let(:habit_plan) { create(:habit_plan) }
     let(:user) { habit_plan.user }
     
@@ -22,7 +22,7 @@ RSpec.describe HabitPlanInviterMailer, type: :mailer do
 
     it "sends the email" do
       expect do
-        described_class.habit_plan_invite_email(user, habit_plan.id, "friend@example.com")
+        described_class.invite_email(user, habit_plan.id, "friend@example.com")
       end.to change { ActionMailer::Base.deliveries.count }.by(1)
     end
   end
