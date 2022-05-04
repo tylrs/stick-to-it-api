2122c0a8afea6d53c8f6ef81af376920d02a2445
Taylor Galloway <galloway.taylor@gmail.com>
2022-05-04 17:31:55 -0500

    Add headers to mailer method

--- app/mailers/habit_plan_inviter_mailer.rb
+++ app/mailers/habit_plan_inviter_mailer.rb
@@ -1,2 +1,9 @@
 class HabitPlanInviterMailer < ApplicationMailer
+  def plan_invite_email(user, habit_plan, friend_email)
+    mail(
+      reply_to: user.email,
+      to: friend_email,
+      subject: "#{user.name} wants you to join their habit: #{habit_plan.habit.name}"
+    )
+  end
 end
--- spec/mailers/habit_plan_inviter_spec.rb
+++ spec/mailers/habit_plan_inviter_spec.rb
@@ -2,25 +2,25 @@ require "rails_helper"
 
 RSpec.describe HabitPlanInviterMailer, type: :mailer do
   describe ".habit_plan_invite_email" do
-    let(:email) {described_class.invite_email(user, habit_plan.id, "friend@example.com")}
+    let(:email) {described_class.plan_invite_email(user, habit_plan, "friend@example.com")}
     let(:habit_plan) { create(:habit_plan) }
     let(:user) { habit_plan.user }
     
     describe "headers" do
       it "has the correct sender" do
-        expect(email.from).to eq "from@example.com"
+        expect(email.from).to eq ["from@example.com"]
       end
       
       it "has the correct recipient" do
-        expect(email.to).to eq "friend@example.com"
+        expect(email.to).to eq ["friend@example.com"]
       end
 
       it "has the correct reply-to" do
-        expect(email.reply_to).to eq "#{user.email}"
+        expect(email.reply_to).to eq ["#{user.email}"]
       end
 
       it "has the correct subject" do
-        expect(email.subject).to eq "#{user.name} wants you to join their habit"
+        expect(email.subject).to eq "#{user.name} wants you to join their habit: #{habit_plan.habit.name}"
       end
     end
 
@@ -30,7 +30,7 @@ RSpec.describe HabitPlanInviterMailer, type: :mailer do
 
     it "sends the email" do
       expect do
-        described_class.invite_email(user, habit_plan.id, "friend@example.com")
+        described_class.plan_invite_email(user, habit_plan.id, "friend@example.com")
       end.to change { ActionMailer::Base.deliveries.count }.by(1)
     end
   end
