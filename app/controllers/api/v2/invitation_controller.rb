7fc564199e9004e8a7122669462163e662ab7a23
Taylor Galloway <galloway.taylor@gmail.com>
2022-05-05 15:08:21 -0500

    Add controller for sending email

--- app/controllers/api/v2/invitation_controller.rb
+++ app/controllers/api/v2/invitation_controller.rb
@@ -1,28 +1,32 @@
-class InvitationController < ApplicationController
-  def create
-    #before action find user, habit plan
+module Api
+  module V2
+    class InvitationController < ApplicationController
+      before_action :find_user, :find_habit_plan
+
+      def create
+        HabitPlanInviterMailer.plan_invite_email(@user, @habit_plan, { name: recipient_params[:name], email: recipient_params[:email] }).deliver_later
+
+        render json: {message: "Email Sent"}, status: :ok
+      end
     
-    #need user, habit_plan, and recipient_info
-    #strong params for recipient info
-    HabitPlanInviterMailer.plan_invite_email
+      private
+    
+      def recipient_params
+        params.permit(:name, :email)
+      end
+    
+      def find_habit_plan
+        @habit_plan = HabitPlan.find params[:habit_plan_id]
+      rescue ActiveRecord::RecordNotFound
+        render json: {errors: "Habit Plan not found"}, status: :not_found
+      end
+    
+      def find_user
+        @user = User.find params[:user_id]
+      rescue ActiveRecord::RecordNotFound
+        render json: { errors: "User not found" }, status: :not_found
+      end
+    
+    end
   end
-
-  private
-
-  def recipient_params
-    params.permit(:name, :email)
-  end
-
-  def find_habit_plan
-    @habit_plan = HabitPlan.find params[:habit_plan_id
-  rescue ActiveRecord::RecordNotFound
-    render json: {errors: "Habit Plan not found"}, status: :not_found
-  end
-
-  def find_user
-    @user = User.find params[:user_id]
-  rescue ActiveRecord::RecordNotFound
-    render json: { errors: "User not found" }, status: :not_found
-  end
-
 end
