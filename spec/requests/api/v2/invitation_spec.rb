2ea10ffc3f31e21d3ea1251ab38b515e7a20456f
Taylor Galloway <galloway.taylor@gmail.com>
2022-05-05 15:07:57 -0500

    Add basic controller test for sending email invite

---
+++ spec/requests/api/v2/invitation_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+RSpec.describe "Invitations v2", type: :request do
+  let(:habit_plan) { create(:habit_plan) }
+  let(:user) { habit_plan.user }
+  let(:token) { JsonWebTokenService.encode(user_id: user.id) }
+  let(:headers) { { "Content-type": "application/json", "Authorization": "Bearer #{token}" } }
+  let(:recipient_info) { { name: "Bob", email: "bob.friend@example.com" } }
+
+  describe "#create" do
+    context "when the email send is successful" do
+      before do
+        post "/api/v2/users/#{user.id}/habit_plans/#{habit_plan.id}/invitation/create",
+             headers: headers,
+             params: JSON.generate(:recipient_info)
+      end
+
+      it "returns http success" do
+        expect(response).to be_ok
+      end
+    end
+
+    context "when the user cannot be found" do
+      before do
+        post "/api/v2/users/500/habit_plans/#{habit_plan.id}/invitation/create",
+             headers: headers,
+             params: JSON.generate(:recipient_info)
+      end
+
+      it "returns a not found status" do
+        expect(response).to have_http_status(:not_found)
+      end
+
+      it "returns an error message" do
+        expect(parsed_response["errors"]).to eq "User not found"
+      end
+    end
+
+    context "when the habit plan cannot be found" do
+      before do
+        post "/api/v2/users/#{user.id}/habit_plans/500/invitation/create",
+             headers: headers,
+             params: JSON.generate(:recipient_info)
+      end
+
+      it "returns a not found status" do
+        expect(response).to have_http_status(:not_found)
+      end
+
+      it "returns an error message" do
+        expect(parsed_response["errors"]).to eq "Habit Plan not found"
+      end
+    end
+  end
+
+end
