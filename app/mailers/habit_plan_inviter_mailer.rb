0a636c8f4deca9522e791811e26d1a90948fff3e
Taylor Galloway <galloway.taylor@gmail.com>
2022-05-05 10:16:26 -0500

    Add instance variables to mailer class

--- app/mailers/habit_plan_inviter_mailer.rb
+++ app/mailers/habit_plan_inviter_mailer.rb
@@ -1,8 +1,13 @@
 class HabitPlanInviterMailer < ApplicationMailer
-  def plan_invite_email(user, habit_plan, friend_email)
+  def plan_invite_email(user, habit_plan, recipient_info)
+    @recipient_name = recipient_info[:name]
+    @user_name = user.name
+    @habit_name = habit_plan.habit.name
+    @start_date = habit_plan.start_datetime
+    @end_date = habit_plan.end_datetime
     mail(
       reply_to: user.email,
-      to: friend_email,
+      to: recipient_info[:email],
       subject: "#{user.name} wants you to join their habit: #{habit_plan.habit.name}"
     )
   end
