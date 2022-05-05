9011df76c28e121afe16f795331d6553270b0651
Taylor Galloway <galloway.taylor@gmail.com>
2022-05-05 10:18:26 -0500

    Add HTML tests

--- spec/mailers/habit_plan_inviter_spec.rb
+++ spec/mailers/habit_plan_inviter_spec.rb
@@ -31,7 +31,14 @@ RSpec.describe HabitPlanInviterMailer, type: :mailer do
         expect(email.text_part.body.to_s).to include("#{user.name} would like you to join them on their new habit: #{habit_plan.habit.name}")
         expect(email.text_part.body.to_s).to include("They are planning on working on the habit from #{habit_plan.start_datetime} to #{habit_plan.end_datetime}")
         expect(email.text_part.body.to_s).to include("If you would like to accept, please click here:")
-      end 
+      end
+      
+      it "returns the correct html" do
+        expect(email.html_part.body.to_s).to include("Hello #{recipient_info[:name]}")
+        expect(email.html_part.body.to_s).to include("#{user.name} would like you to join them on their new habit: #{habit_plan.habit.name}")
+        expect(email.html_part.body.to_s).to include("They are planning on working on the habit from #{habit_plan.start_datetime} to #{habit_plan.end_datetime}")
+        expect(email.html_part.body.to_s).to include("If you would like to accept, please click here:")
+      end
     end
 
     it "sends the email" do
