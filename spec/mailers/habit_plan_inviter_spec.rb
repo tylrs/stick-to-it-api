a3a430326d35788ee661e820e107d921ce0927ec
Taylor Galloway <galloway.taylor@gmail.com>
2022-05-05 10:14:55 -0500

    Add test for plain text template

--- spec/mailers/habit_plan_inviter_spec.rb
+++ spec/mailers/habit_plan_inviter_spec.rb
@@ -2,7 +2,8 @@ require "rails_helper"
 
 RSpec.describe HabitPlanInviterMailer, type: :mailer do
   describe ".habit_plan_invite_email" do
-    let(:email) {described_class.plan_invite_email(user, habit_plan, "friend@example.com")}
+    let(:recipient_info) {{name: "Bob", email: "bob.friend@example.com"}}
+    let(:email) {described_class.plan_invite_email(user, habit_plan, recipient_info)}
     let(:habit_plan) { create(:habit_plan) }
     let(:user) { habit_plan.user }
     
@@ -12,7 +13,7 @@ RSpec.describe HabitPlanInviterMailer, type: :mailer do
       end
       
       it "has the correct recipient" do
-        expect(email.to).to eq ["friend@example.com"]
+        expect(email.to).to eq ["#{recipient_info[:email]}"]
       end
 
       it "has the correct reply-to" do
@@ -25,12 +26,17 @@ RSpec.describe HabitPlanInviterMailer, type: :mailer do
     end
 
     describe "body" do
-      
+      it "returns the correct plain text" do
+        expect(email.text_part.body.to_s).to include("Hello #{recipient_info[:name]}")
+        expect(email.text_part.body.to_s).to include("#{user.name} would like you to join them on their new habit: #{habit_plan.habit.name}")
+        expect(email.text_part.body.to_s).to include("They are planning on working on the habit from #{habit_plan.start_datetime} to #{habit_plan.end_datetime}")
+        expect(email.text_part.body.to_s).to include("If you would like to accept, please click here:")
+      end 
     end
 
     it "sends the email" do
       expect do
-        described_class.plan_invite_email(user, habit_plan, "friend@example.com").deliver_now
+        described_class.plan_invite_email(user, habit_plan, recipient_info).deliver_now
       end.to change { ActionMailer::Base.deliveries.count }.by(1)
     end
   end
