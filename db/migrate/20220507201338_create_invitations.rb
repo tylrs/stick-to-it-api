bf8e2d620090543ea1d500b55c15a709fe2409c8
Taylor Galloway <galloway.taylor@gmail.com>
2022-05-09 12:40:03 -0500

    Run migration to add invitation table

--- db/migrate/20220507201338_create_invitations.rb
+++ db/migrate/20220507201338_create_invitations.rb
@@ -1,14 +1,18 @@
 class CreateInvitations < ActiveRecord::Migration[7.0]
   def change
     create_table :invitations do |t|
-      t.belongs_to :user, null: false, foreign_key: true
-      t.bigint :recipient_id
+      t.bigint :sender_id, null: false
+      t.bigint :recipient_id, null: false
       t.string :recipient_email, null: false
-      t.belongs_to :habit_plan, null: false, foreign_key: true
+      t.belongs_to :habit_plan, null: false
       t.integer :status
 
       t.timestamps
     end
-    add_index :invitations, :recipient_email
+    add_index :invitations, :sender_id
+    add_index :invitations, :recipient_id
+    add_foreign_key :invitations, :users, column: "sender_id"
+    add_foreign_key :invitations, :users, column: "recipient_id"
+    add_foreign_key :invitations, :habit_plans
   end
 end
