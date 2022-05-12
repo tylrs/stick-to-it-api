abcaa838d4bb730524e3ed826d0dc22cdfcb35dd
Taylor Galloway <galloway.taylor@gmail.com>
2022-05-12 09:22:37 -0500

    Add template and methods for guest invite mailer

--- app/mailers/habit_plan_inviter_mailer.rb
+++ app/mailers/habit_plan_inviter_mailer.rb
@@ -5,7 +5,20 @@ class HabitPlanInviterMailer < ApplicationMailer
     @habit_name = habit_plan.habit.name
     @start_date = habit_plan.start_datetime
     @end_date = habit_plan.end_datetime
-    @accept_invite_url = "URLTOBEGENERATED"
+    
+    mail(
+      reply_to: user.email,
+      to: recipient_info[:email],
+      subject: "#{user.name} wants you to join their habit: #{habit_plan.habit.name}"
+    )
+  end
+
+  def plan_invite_guest_email(user, habit_plan, recipient_info)
+    @recipient_name = recipient_info[:name]
+    @user_name = user.name
+    @habit_name = habit_plan.habit.name
+    @start_date = habit_plan.start_datetime
+    @end_date = habit_plan.end_datetime
     
     mail(
       reply_to: user.email,
--- spec/mailers/previews/habit_plan_inviter_preview.rb
+++ spec/mailers/previews/habit_plan_inviter_preview.rb
@@ -3,4 +3,8 @@ class HabitPlanInviterPreview < ActionMailer::Preview
   def plan_invite_user_email
     HabitPlanInviterMailer.plan_invite_user_email(User.first, HabitPlan.first, { name: "Bob", email: "bob.friend@example.com" })
   end
+
+  def plan_invite_guest_email
+    HabitPlanInviterMailer.plan_invite_guest_email(User.first, HabitPlan.first, { name: "John", email: "john.newfriend@example.com" })
+  end
 end
