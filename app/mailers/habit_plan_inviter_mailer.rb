19834fdf654e7db77cd9b771d36558662aba1a16
Taylor Galloway <galloway.taylor@gmail.com>
2022-05-12 14:10:24 -0500

    Revert to one mailer method

--- app/mailers/habit_plan_inviter_mailer.rb
+++ app/mailers/habit_plan_inviter_mailer.rb
@@ -1,29 +1,23 @@
 class HabitPlanInviterMailer < ApplicationMailer
-  def plan_invite_user_email(user, habit_plan, recipient_info)
-    @recipient_name = recipient_info[:name]
-    @user_name = user.name
-    @habit_name = habit_plan.habit.name
-    @start_date = habit_plan.start_datetime
-    @end_date = habit_plan.end_datetime
-    
-    mail(
-      reply_to: user.email,
-      to: recipient_info[:email],
-      subject: "#{user.name} wants you to join their habit: #{habit_plan.habit.name}"
-    )
-  end
+  def plan_invite_email(invite_info)
+    @recipient_name = invite_info[:recipient_name]
+    @user_name = invite_info[:user].name
+    @habit_name = invite_info[:habit_plan].habit.name
+    @start_date = invite_info[:habit_plan].start_datetime
+    @end_date = invite_info[:habit_plan].end_datetime
 
-  def plan_invite_guest_email(user, habit_plan, recipient_info)
-    @recipient_name = recipient_info[:name]
-    @user_name = user.name
-    @habit_name = habit_plan.habit.name
-    @start_date = habit_plan.start_datetime
-    @end_date = habit_plan.end_datetime
+    if User.find_by email: invite_info[:recipient_email]
+      @invitation_link = "http://stick-to-it-ui.herokuapp.com/login"
+      @action_type = "log in"
+    else
+      @invitation_link = "http://stick-to-it-ui.herokuapp.com/create-account"
+      @action_type = "create an account"
+    end
     
     mail(
-      reply_to: user.email,
-      to: recipient_info[:email],
-      subject: "#{user.name} wants you to join their habit: #{habit_plan.habit.name}"
+      reply_to: invite_info[:user].email,
+      to: invite_info[:recipient_email],
+      subject: "#{invite_info[:user].name} wants you to join their habit: #{invite_info[:habit_plan].habit.name}"
     )
   end
 end
