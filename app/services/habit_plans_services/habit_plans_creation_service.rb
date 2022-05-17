8fab7145383d938b2d51c0cd855fe62aa14b3893
Taylor Galloway <galloway.taylor@gmail.com>
2022-05-17 14:51:43 -0500

    Remove bang operator

--- app/services/habit_plans_services/habit_plans_creation_service.rb
+++ app/services/habit_plans_services/habit_plans_creation_service.rb
@@ -1,7 +1,7 @@
 module HabitPlansCreationService
   def self.create_partner_plans(habit_plan_id, recipient_id)
     sender_habit_plan = HabitPlan.find habit_plan_id
-    recipient_habit_plan = HabitPlan.create!(sender_habit_plan.attributes.merge(id: nil, user_id: recipient_id))
+    recipient_habit_plan = HabitPlan.create(sender_habit_plan.attributes.merge(id: nil, user_id: recipient_id))
     HabitLogsCreationService.create_initial_logs(recipient_habit_plan)
   end
 end
