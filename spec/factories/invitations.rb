5124752c28180e9224c643a0b4eb39e59689f638
Taylor Galloway <galloway.taylor@gmail.com>
2022-05-20 10:19:14 -0500

    Remove nested association

--- spec/factories/invitations.rb
+++ spec/factories/invitations.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :invitation do
     sender
     recipient_email { Faker::Internet.safe_email }
-    habit_plan { association :habit_plan, invitation: instance, user: sender }
+    habit_plan
     status { 0 }
   end
 end
